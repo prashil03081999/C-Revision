1014005e791f0d25172ce9950968b3b5aa00f92c
Prashil Ramteke <87717960+prashil03081999@users.noreply.github.com>
2021-11-02 12:33:07 +0530

    tut15

---
+++ tut15.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    int i, j=0;
+    for(i=0 ; i<10 ;i++ )
+    {
+       printf("%d %d\n", i, j);
+       i++;j++;
+    }
+    
+    return 0;
+}
+
